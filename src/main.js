0308abebd495656db9997e56a5b15c347b471031
Durul Doktoroglu <durul@sufle.io>
2021-01-20 00:40:28 +0300

    fix package issues

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import store from './store'
 
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsconfig from './aws-exports'
 
-Amplify.configure(awsconfig)
+//import awsconfig from './aws-exports'
+//Amplify.configure(awsconfig)
+
 Vue.use(AmplifyPlugin, AmplifyModules)
 Vue.config.productionTip = false
 
